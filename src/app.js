48b3d5adb00502f1586118c67b4b03f764d7ac62
DamienDemontis <damien.demontis@epitech.eu>
2023-06-04 20:21:24 +0200

    [ADD] Documentation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const app = express();
 
 // load the environment variables from .env 
@@ -8,6 +10,35 @@ require('dotenv').config();
 
 // Connect Database
 connectDB();
+
+// Swaggers
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'LeonArt API',
+      version: '1.0.0',
+      description: 'API documentation for the LeonArt project',
+      contact: {
+        name: 'LeonArt Team',
+        email: 'leonarteip@epitechfr.onmicrosoft.com',
+      },
+      servers: [
+        {
+          url: 'http://localhost:5000/api',
+          description: 'Development server',
+        },
+      ],
+    },
+  },
+  apis: ['./src/routes/*.js', './src/controllers/*.js']
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
+
 
 // Init Middleware
 app.use(express.json({ extended: false }));
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,93 @@ const signupController = require('../controllers/signupController');
 const loginController = require('../controllers/loginController');
 const validate = require('../middleware/validation');
 
+
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Register a new user
+ *     description: Register a new user in the system by username, email, and password. Returns a JWT token if successful.
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The user's username.
+ *               email:
+ *                 type: string
+ *                 description: The user's email.
+ *               password:
+ *                 type: string
+ *                 description: The user's password.
+ *             example:
+ *               username: user1
+ *               email: user1@example.com
+ *               password: user1Password
+ *     responses:
+ *       200:
+ *         description: The user was successfully created.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: JWT token for the registered user.
+ *       409:
+ *         description: The email is already in use.
+ *       422:
+ *         description: The input is invalid.
+ *       500:
+ *         description: Server Error.
+ */
 router.post('/signup', validate.validateSignup, signupController.signup);
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login a user
+ *     description: Login a user into the system by email and password. Returns a JWT token if successful.
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The user's email.
+ *               password:
+ *                 type: string
+ *                 description: The user's password.
+ *             example:
+ *               email: user1@example.com
+ *               password: user1Password
+ *     responses:
+ *       200:
+ *         description: The user was successfully logged in.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: JWT token for the logged in user.
+ *       401:
+ *         description: Incorrect password or email not registered.
+ *       500:
+ *         description: Server Error.
+ */
 router.post('/login', loginController.login);
 
 module.exports = router;

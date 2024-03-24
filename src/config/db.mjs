a689154252ef811ab33c75abe3ef0f540827dd2d
Cédric Ulrich <cedric.ulrich@epitech.eu>
2024-03-24 12:52:15 +0100

    [ADD] Setup firebase connexion

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -1,15 +1,21 @@
-import mongoose from "mongoose";
+import admin from 'firebase-admin';
+// Notez l'ajout de "assert { type: 'json' }" à la fin de l'importation
+import serviceAccount from '../../leon-art-d8942-firebase-adminsdk-zkdcu-115fd03559.json'assert { type: 'json' };
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: "leonart",
+    // Initialisez l'application Firebase Admin avec les informations d'authentification
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
     });
-    console.log("MongoDB connected...");
+
+    console.log("Firebase connected...");
+
+    // Obtenez une référence à la base de données Firestore
+    const db = admin.firestore();
+    return db; // Retournez la référence pour l'utiliser dans votre application
   } catch (err) /* istanbul ignore next */ {
-    console.error(err.message);
+    console.error("Error connecting to Firebase:", err.message);
     process.exit(1);
   }
 };
--- src/models/userModel.mjs
+++ src/models/userModel.mjs
@@ -1,25 +1,19 @@
-import mongoose from "mongoose";
-
-const { Schema } = mongoose;
-
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  is_artist: { type: Boolean, default: false },
-  biography: String,
-  availability: { type: String, default: "unavailable" },
-  subscription: { type: String, default: "standard" },
-  collections: [{ type: Schema.Types.ObjectId, ref: 'Collection' }],
-  subscriptions: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  subscribers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  subscribersCount: { type: Number, default: 0 },
-  likedPublications: [{ type: Schema.Types.ObjectId, ref: 'ArtPublication' }],
-  canPostArticles: { type: Boolean, default: true },
-  fcmToken: String, // FCM token for push notifications
-  profilePicture: { type: String, default: 'uploads/static/default-profile-pic.png' },
-  bannerPicture: { type: String, default: 'uploads/static/default-banner-pic.png' },
-  stripeAccountId: { type: String },
-});
-
-export const User = mongoose.model("User", userSchema);
+export const User = (
+  username = "",
+  email = "",
+  password = "",
+  is_artist = false,
+  biography = "",
+  availability = "unavailable",
+  subscription = "standard",
+  collections = [],
+  subscriptions = [],
+  subscribers = [],
+  subscribersCount = 0,
+  likedPublications = [],
+  canPostArticles = true,
+  fcmToken = "",
+  profilePicture = "uploads/static/default-profile-pic.png",
+  bannerPicture = "uploads/static/default-banner-pic.png",
+  stripeAccountId = ""
+)

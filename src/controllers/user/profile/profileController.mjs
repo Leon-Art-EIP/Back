00ee417311f856999754bea6127c0676f35a46bf
Cédric Ulrich <cedric.ulrich@epitech.eu>
2024-05-25 11:59:39 +0200

    [UPDATE] Fixing the update profile method

--- src/controllers/user/profile/profileController.mjs
+++ src/controllers/user/profile/profileController.mjs
@@ -29,25 +29,30 @@ export const updateBiography = async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(userId, { biography });
 
     // Manuellement exclure les champs sensibles
-    const userWithoutSensitiveInfo = { ...updatedUser };
+    const userWithoutSensitiveInfo = updatedUser.toJSON();
     delete userWithoutSensitiveInfo.password;
     delete userWithoutSensitiveInfo.email;
 
     res.json(userWithoutSensitiveInfo);
-  } catch (err) /* istanbul ignore next */ {
+  } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg: "Server Error" });
   }
 };
 
-
 export const updateAvailability = async (req, res) => {
   try {
     const userId = req.user.id;
     const { availability } = req.body;
-    const user = await User.findByIdAndUpdate(userId, { availability }, { new: true }).select("-password -email");
-    res.json(user);
-  } catch (err) /* istanbul ignore next */ {
+    const updatedUser = await User.findByIdAndUpdate(userId, { availability }, { new: true });
+
+    // Manuellement exclure les champs sensibles
+    const userWithoutSensitiveInfo = updatedUser.toJSON();
+    delete userWithoutSensitiveInfo.password;
+    delete userWithoutSensitiveInfo.email;
+
+    res.json(userWithoutSensitiveInfo);
+  } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg: "Server Error" });
   }
@@ -57,9 +62,15 @@ export const updateProfilePicture = async (req, res) => {
   try {
     const userId = req.user.id;
     const profilePicture = req.file.path; // getting file path from multer
-    const user = await User.findByIdAndUpdate(userId, { profilePicture }, { new: true }).select("-password -email");
-    res.json(user);
-  } catch (err) /* istanbul ignore next */ {
+    const updatedUser = await User.findByIdAndUpdate(userId, { profilePicture }, { new: true });
+
+    // Manuellement exclure les champs sensibles
+    const userWithoutSensitiveInfo = updatedUser.toJSON();
+    delete userWithoutSensitiveInfo.password;
+    delete userWithoutSensitiveInfo.email;
+
+    res.json(userWithoutSensitiveInfo);
+  } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg: "Server Error" });
   }
@@ -69,9 +80,15 @@ export const updateBannerPicture = async (req, res) => {
   try {
     const userId = req.user.id;
     const bannerPicture = req.file.path; // getting file path from multer
-    const user = await User.findByIdAndUpdate(userId, { bannerPicture }, { new: true }).select("-password -email");
-    res.json(user);
-  } catch (err) /* istanbul ignore next */ {
+    const updatedUser = await User.findByIdAndUpdate(userId, { bannerPicture }, { new: true });
+
+    // Manuellement exclure les champs sensibles
+    const userWithoutSensitiveInfo = updatedUser.toJSON();
+    delete userWithoutSensitiveInfo.password;
+    delete userWithoutSensitiveInfo.email;
+
+    res.json(userWithoutSensitiveInfo);
+  } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg: "Server Error" });
   }
